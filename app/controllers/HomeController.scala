850dd7e691a85712e9adfb60b77294535993be45
Sajit Gupta <sajit.gupta@knoldus.in>
2017-08-05 00:29:12 +0530

    Play-01-assignment

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,41 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+/**
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
+@Singleton
+class HomeController extends Controller {
+
+  /**
+    * Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
+  def index() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
+
+  def setSessionAction() = Action { request: Request[AnyContent] =>
+
+    Ok("<h1>Session created<h1>").as(HTML).withSession("user" -> "sajit gupta")
+  }
+
+  def getSessionAction = Action { request: Request[AnyContent] =>
+
+    val userOption = request.session.get("user")
+
+    userOption match {
+      case Some(data) => Redirect(routes.HomeController.flashData()).flashing("message" -> s"$data has sucessfully logged in.")
+      case None => Redirect(routes.HomeController.flashData()).flashing("message" -> "Failure")
+    }
+  }
+
+  def flashData() = Action { request: Request[AnyContent] => Ok(request.flash.get("message").get).as(HTML) }
+}
